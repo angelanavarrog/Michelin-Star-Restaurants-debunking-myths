84b2b9be716066808d6e0868bddc9b7b23b7a47e
Ángela <angela.navarro.gonzalez@gmail.com>
2021-03-10 14:06:12 +0100

    streamlit

--- main.py
+++ main.py
@@ -2,37 +2,72 @@ import streamlit as st
 import src.manage_data as dat
 import plotly.express as px
 from PIL import Image
+import folium
+from streamlit_folium import folium_static
+
 
 imagen = Image.open("images/image1.jpg")
+
+# Portada y título
 
 st.image(imagen)
 st.write( """
 # Spanish Michelin awarded restaurant app"""
-
 )
 
+# Dataframe restaurants database
 st.write( """
 ### Restaurants dataframe""")
 
 restaurants = dat.charge_data()
 st.dataframe(dat.charge_data())
 
+st.write("""
+## ** Hypothesis 1: "Awarded restaurants are equally distributed per Spanish regions.**
+""")
+
+
+# Dataframe that matches regions and number of Michelin Star associated
 st.write( """
-### How are Michelin Stars distributed per region""")
+### How are Michelin Stars distributed per region?""")
 graph1 = dat.bar_chart()
-st.dataframe(graph1)
-
-region = st.selectbox(
-    "Choose a region",dat.region_list())
-
-
-datapragh = dat.graph(region)
-fig = px.bar(datapragh, y = "michelin_stars", title = f"Amount of restaurants in {region}",
-labels = {"index": "region"}
-)
 st.bar_chart(graph1)
 
 
+st.write("""***Hypothesis number 1 is not true: the distribution of restaurants per region is not equal.***
+
+The total amount of awarded resturants in Spain is of 220.
+
+At the top end is ***Catalonia, with 54 (around 25% of the restaurants)***. At the other end of the scale is ***Extremadura, with just one*** award-winning restaurant, it represents a ***0.04%***.
+
+Is there any relation with the population of each region?
+""")
+
+st.write( """### Restaurants located in Spain map""")
+# Incluir mapa con restaurantes representados
+st.map(dat.charge_data())
+
+
+# Region select box
+region = st.selectbox(
+    "Choose a region",dat.region_list())
+st.write("You selected:",region)
+
+
+# Bar graph that shows the province restaurants distribution 
+datapragh = dat.graph2(region)
+fig1 = px.bar(datapragh, x = "michelin_stars", y = "region", title = f"Amount of restaurants in {region}",
+labels = {"index": "restaurant"},
+)
+st.plotly_chart(fig1)
+# Pie chart to show regional distribution per number of stars
+st.write( """
+### Restaurant awarded distribution""")
+fig2 = px.pie(dat.graph2(region), "michelin_stars")
+st.plotly_chart(fig2)
+
+
+# Dataframe restaurants per province database and graph distribution
 st.write( """
 ### How are Michelin Stars distributed per province?""")
 graph2 = dat.bar_chart_st()
@@ -40,19 +75,23 @@ st.dataframe(graph2)
 
 st.bar_chart(graph2)
 
+# Province select box
 province = st.selectbox(
     "Choose a province",dat.province_list())
 st.write("You selected:",province)
 
+# Bar graph that shows the province restaurants distribution 
 datapragh = dat.graph(province)
-fig = px.bar(datapragh, y = "michelin_stars", title = f"Amount of restaurants in {province}",
+fig3 = px.bar(datapragh, y = "michelin_stars", title = f"Amount of restaurants in {province}",
 labels = {"index": "restaurant"}
 )
-st.plotly_chart(fig)
+st.plotly_chart(fig3)
 
+st.write( """### Michelin Stars per region and province""")
 
-
-st.write( """
-### Province: populationd and employment and unemployment rates""")
-province_data = dat.charge_data2()
-st.dataframe(dat.charge_data2())
+fig4 = px.sunburst(
+    dat.charge_data(),
+    path=['region', 'province'],
+    values = 'michelin_stars',
+)
+st.plotly_chart(fig4)
--- src/manage_data.py
+++ src/manage_data.py
@@ -1,10 +1,12 @@
 import pandas as pd
 
 def charge_data():
+    #Function to obtain restaurants database info
     restaurants = pd.read_csv('output/restaurants.csv')
     return restaurants
 
 def charge_data2():
+    #Function to obtain province database info
     province_data = pd.read_csv('output/province_data.csv')
     return province_data
 
@@ -12,41 +14,45 @@ def rename_id(x):
     x = f"Province {x}"
     return x
 
-def rename_r(x):
+"""def rename_r(x):
     x = f"Region {x}"
-    return x
+    return x"""
 
 def bar_chart():
+    #Function to represent regional Michelin Star information
     data = charge_data()
     data = data.groupby("region").agg({"michelin_stars":'count'}).rename(columns = {"region":"region","region":"Total Michelin Stars"}).reset_index().set_index("region",drop = True)
     return data
 
 def bar_chart_st():
+    #Function to represent proovince Michelin Star information
     data = charge_data()
     data = data.groupby("province").agg({"michelin_stars":'count'}).rename(columns = {"province":"province","province":"Total Michelin Stars"}).reset_index().set_index("province",drop = True)
     return data
 
 def province_list():
+    #Function to obtain unique province
     data = charge_data()
     return list(data.province.unique())
 
 def region_list():
+    #Function to obtain unique region names
     data = charge_data()
     return list(data.region.unique())
 
-def graph(region):
+def graph2(region):
     data = charge_data()
     data = data[(data["region"]== f"{region}")]
-    data = data[["region","michelin_stars"]].reset_index(drop=True)
-    data["region"]= data.index+1
-    data["region"]= data.province.apply(rename_r)
     return data
 
 def graph(province):
     data = charge_data()
     data = data[(data["province"]== f"{province}")]
-    data = data[["province","michelin_stars"]].reset_index(drop=True)
-    data["province"]= data.index+1
-    data["province"]= data.province.apply(rename_id)
     return data
+
+def chart_rates():
+    data = charge_data2()
+    data = data.groupby("Provincias").agg({"Unemployment rate":'sum'}).rename(columns = {"Provincias":"Provincias","Provincias":"Unemployment rate"}).reset_index().set_index("Provincias",drop = True)
+    return data
+
 
